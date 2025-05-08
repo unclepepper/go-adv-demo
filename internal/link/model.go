d63f9724ac935713335d0bfbc2192765f729a718
Nikolay Terleev <unclepepper.dev@gmail.com>
2025-05-08 16:01:32 +0300

    feat: Create model, migration

--- internal/link/model.go
+++ internal/link/model.go
@@ -1,6 +1,29 @@
 package link
 
+import (
+	"gorm.io/gorm"
+	"math/rand"
+)
+
 type Link struct {
+	gorm.Model
 	URL  string `json:"url"`
-	Hash string `json:"hash"`
+	Hash string `json:"hash" gorm:"uniqueIndex"`
+}
+
+func NewLink(url string) (*Link, error) {
+	return &Link{
+		URL:  url,
+		Hash: RandStringRunes(6),
+	}, nil
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
---
+++ migrations/auto.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"go/adv-demo/internal/link"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	err = db.AutoMigrate(link.Link{})
+	if err != nil {
+		return
+	}
+}
