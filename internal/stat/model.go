afcd97317ba218d505af12ef9d13a62fb46466df
Nikolay Terleev <unclepepper.dev@gmail.com>
2025-05-12 09:13:24 +0300

    feat: Create repository Stat

--- internal/stat/model.go
+++ internal/stat/model.go
@@ -1,13 +1,13 @@
 package stat
 
 import (
-	"github.com/jackc/pgx/v5/pgtype"
+	"gorm.io/datatypes"
 	"gorm.io/gorm"
 )
 
 type Stat struct {
 	gorm.Model
-	LinkId uint        `json:"link_id"`
-	Clicks int         `json:"clicks"`
-	Date   pgtype.Date `json:"date"`
+	LinkId uint           `json:"link_id"`
+	Clicks int            `json:"clicks"`
+	Date   datatypes.Date `json:"date"`
 }
